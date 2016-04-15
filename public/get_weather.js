c3c7d949863f15acbbf1433e4066f8485c48a0e3
Jared <jaredcmurphy@gmail.com>
2016-04-15 13:53:55 -0400

    prepends data to dom, so new data is at top

--- public/get_weather.js
+++ public/get_weather.js
@@ -19,24 +19,19 @@ document.addEventListener("DOMContentLoaded", function() {
   } // ends fetchWeather function
 
   var addToDom = function(data) {
-    var city = document.createElement("p");
-    city.textContent= data.name;
-    city.id = "city";
-    document.body.appendChild(city);
     // below sets up data object
-
+    var dataContainer = document.getElementById('dataLists');
     var cityWeatherList = document.createElement("ul");
-    document.body.appendChild(cityWeatherList);
-
+    dataContainer.insertBefore(cityWeatherList, dataContainer.firstChild)
+    // below adds data to dom 
     for (key in data) {
-      console.log(key + " " + data[key])
       var weatherDataPoint = document.createElement("li");
       weatherDataPoint.textContent = key + ": " + data[key];
       weatherDataPoint.id = key;
       cityWeatherList.appendChild(weatherDataPoint);
     }
     
-  }
+  } // ends add to dom function
   
 
   var apiCall = function(city) {
@@ -44,31 +39,24 @@ document.addEventListener("DOMContentLoaded", function() {
     // calling fetch data function
     fetchWeather(link, function(data) {
       var weatherData = {};
+      weatherData.city = data.name
       weatherData.humidity = data.main.humidity;
       weatherData.temp = data.main.temp;
       weatherData.description = data.weather[0].description;
       weatherData.wind = data.wind.speed;
-      console.log(weatherData);
       addToDom(weatherData)
 
-      //console.log(data);
-    }) // ends apiCall function
-  } 
+    }) // ends feetchWeather function
+  } // ends apiCall function
 
   var input = document.getElementById('city');
   var submitButton = document.getElementById('submit');
 
   submitButton.onclick = function() {
     apiCall(input.value);
-
-  };
-
-
-
-  
-
-
-});
+    input.value = "";
+  }; // ends submit button function
+}); // ends document dom loaded function
 
 
 
