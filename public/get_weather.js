b275dbeda842fd2d999e1b5212dd06f9662a1ee1
Jared <jaredcmurphy@gmail.com>
2016-04-15 00:26:15 -0400

    stores weather data in js object

--- public/get_weather.js
+++ public/get_weather.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // function to call api data
   function fetchWeather(path, callback) {
-    console.log('fectch weather function started');
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
       if (httpRequest.readyState === 4) {
@@ -18,34 +17,45 @@ document.addEventListener("DOMContentLoaded", function() {
     httpRequest.open("GET", path);
     httpRequest.send();
   } // ends fetchWeather function
+
+  var addToDom = function(data) {
+    var city = document.createElement("p");
+    city.textContent= data.name;
+    city.id = "city";
+    document.body.appendChild(city);
+    // below sets up data object
+    var weatherData = {};
+    weatherData.humidity = data.main.humidity;
+    weatherData.temp = data.main.temp;
+    weatherData.rain = data.rain['1h'];
+    weatherData.description = data.description.description;
+    weatherData.wind = data.wind.speed;
+    console.log(weatherData);
+
+    var cityWeatherList = document.createElement("ul");
+
+    for (key in weatherData) {
+      console.log(key + " " + weatherData[key])
+    }
+    
+  }
   
 
   var apiCall = function(city) {
-    console.log('apiCall function started')
     var link = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=45683852bbe910d7d9147f78f770c0b7";
     // calling fetch data function
     fetchWeather(link, function(data) {
-      console.log(data);
+      addToDom(data)
+      //console.log(data);
     }) // ends apiCall function
   } 
 
-
-  
-
-  console.log('get_weather.js loaded');
   var input = document.getElementById('city');
   var submitButton = document.getElementById('submit');
 
   submitButton.onclick = function() {
-    console.log('button was clicked: ' + input.value);
-
     apiCall(input.value);
 
-    
-  //var test = document.createElement("p");
-  //test.textContent= input.value;
-  //test.id = "test";
-  //document.body.appendChild(test);
   };
 
 
